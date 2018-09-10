17ee5c06eac95d9faab65e9e748f0e0ed80673cf
Josh Zook <github@joshzook.com>
2018-09-10 18:16:13 -0400

    changed to stop overwriting snapshots taken in the deploy with test env imaged

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ export TZ='America/New_York'
 cd /apps/snapshots
 
 pkill -f SeasonalSnapshots
-unzip -oq seasonalsnapshots-0.0.1-SNAPSHOT.jar
+unzip -oq seasonalsnapshots-0.0.1-SNAPSHOT.jar -x "*/snapshots/*"
 java -Dspring.profiles.active=prod -cp ./:./lib/*:./:./BOOT-INF/classes:./BOOT-INF/lib/* com.chubbybuttons.seasonalsnapshots.SeasonalSnapshotsApplication
